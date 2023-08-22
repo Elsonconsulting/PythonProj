d11dc3c4908ddb746776287d0bb9f852c56e8462
Elsonconsulting <97523590+Elsonconsulting@users.noreply.github.com>
2023-08-22 21:36:54 +0100

    chatgpt code

--- BlackJack/deck.py
+++ BlackJack/deck.py
@@ -1,3 +1,7 @@
 class CardDeck:
+    def __init__(self):
+        self.cards = [S, D, C, H], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
+
     def cardRnd():
-        cards = [S, D, C, H] [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
+        print('end')
+        # code here
